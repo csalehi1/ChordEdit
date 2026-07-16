4d23936c32d8f3ea0b4d2eb9fb459e8ffe88334d
Daniel Mirick <dmirick@college.harvard.edu>
2026-07-16 15:26:06 -0400

    Include generation and evaluation scripts for GPU usage

--- daniel_grid_optimizations/grid_run.sh
+++ daniel_grid_optimizations/grid_run.sh
@@ -1,2 +1,7 @@
-# Test script for grid_generate.py: run two GPU shards to generate <--max-samples> samples.
-# python grid_generate.py --data-root /shared/ssd_30T/mirick/datasets/ultra_edit/UltraEdit_Region_100 --output-root /shared/ssd_30T/mirick/generated/ultra_edit --model-root /shared/ssd_30T/mirick/models/sd-turbo --gpus 0 1  --max-samples 4
+# Test scripts for grid_generate.py and grid_eval.py.
+
+# Generate grids for the first 10 samples across 2 GPUs.
+python grid_generate.py --data-root /shared/ssd_30T/mirick/datasets/ultra_edit/UltraEdit_Region_100 --model-root /shared/ssd_30T/mirick/models/sd-turbo --gpus 0 1 --max-samples 10
+
+# Evaluate the grids for the first 10 samples across 2 GPUs.
+python grid_eval.py --generated-root /data/home/mirick/ChordEdit/daniel-grid-optimizations/daniel_grid_optimizations/generated/UltraEdit_Region_100_n10 --include-psnr --include-lpips --include-clip --gpus 0 1
